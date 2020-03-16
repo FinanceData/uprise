369f22e23d0471d9cf32f2468509cc5eff896f95
financedata <plusjune@gmail.com>
2020-03-16 05:41:00 +0000

    first commit

---
+++ git_push.sh
@@ -0,0 +1,6 @@
+touch b.txt
+git config --global user.email "plusjune@gmail.com"
+git config --global user.name "financedata"
+git add .
+git commit -m 'first commit'
+git push https://financedata:8k99sm8k99sm@github.com/financedata/uprise.git master

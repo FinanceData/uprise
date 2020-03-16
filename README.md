f84c8584135bda34ff5282121f362b1498fe7cc3
financedata <plusjune@gmail.com>
2020-03-16 07:59:38 +0000

    update data

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # uprise stocks daily
-2006-09-06 ~ current uprise stocks dataset
+2015-07-24 ~ 2020-02-06 uprise stocks dataset
 
 #### 2020 [FinanceData.KR](http://financedata.kr) | [facebook.com/financedata](http://facebook.com/financedata)
---
+++ uprise_push.sh
@@ -0,0 +1,7 @@
+cd uprise
+git config --global user.email "plusjune@gmail.com"
+git config --global user.name "financedata"
+git add .
+git commit -m 'update data'
+git push https://financedata:8k99sm8k99sm@github.com/financedata/uprise.git master
+cd ..
